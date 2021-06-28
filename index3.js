6e7d165f631d573b5c58e58b0fd26916d1a0693d
Yucas <63893751+yukayuca@users.noreply.github.com>
2021-06-28 23:59:44 +0900

    Create index3.js

---
+++ index3.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import ColorfulMsg from "./components/ColorfulMsg";
+import CountButton from "./components/CountButton";
+
+const App = () => {
+  return (
+    <>
+      <h2>Reactハンズオン！</h2>
+      <ColorfulMsg color="blue">Reactは難しくないよー</ColorfulMsg>
+      <ColorfulMsg color="green">Vueも難しくないよー</ColorfulMsg>
+      <ColorfulMsg color="red">Angularは、、、</ColorfulMsg>
+      <CountButton />
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
